736f259fa5b5656f8837f675002156c67a0543bf
Stas Perchenko <sperchenko@gmail.com>
2015-11-30 01:26:33 +0200

    Creating users

--- app/src/main/java/com/sperchenko/likedislikedemo/storage/DBUtils.java
+++ app/src/main/java/com/sperchenko/likedislikedemo/storage/DBUtils.java
@@ -1,5 +1,10 @@
 package com.sperchenko.likedislikedemo.storage;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.sperchenko.likedislikedemo.model.Person;
+
 /**
  * Created by Stas on 29.11.2015.
  */
@@ -11,6 +16,22 @@ public class DBUtils {
         } catch (InterruptedException e) {
 
         }
+
+
+        createPerson(helper, 2457, "testUser1", "pass1", "mr. John Smith", 100);
+        createPerson(helper, 2460, "testUser2", "1234", "Silvia Blue", 101);
+        createPerson(helper, 2471, "qwerty@mail.com", "0000", "Amanda Hollower", 102);
+
+    }
+
+    private static void createPerson(@NonNull SQLiteDataHelper helper, int id, @NonNull String userName, @Nullable String password, @NonNull String displayedName, int photoId) {
+        Person p = new Person();
+        p.setId(id);
+        p.setUserName(userName);
+        p.setPassword(password);
+        p.setDisplayedName(displayedName);
+        p.setPhotoId(photoId);
+        helper.getPersonDAO().createIfNotExists(p);
     }
 
 }
